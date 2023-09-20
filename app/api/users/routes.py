ee8b5f478c4137ad170a79d11da2b147186685fc
ФÐArtem <goldaev51@gmail.com>
2023-09-20 14:30:08 +0300

    change for endpoint update_user_me and delete init_db.py

--- app/api/users/routes.py
+++ app/api/users/routes.py
@@ -21,10 +21,10 @@ def alive():
 
 @router.get("/", response_model=List[schemas.User])
 def read_users(
-    db: Session = Depends(deps.get_db),
-    skip: int = 0,
-    limit: int = 100,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
+        db: Session = Depends(deps.get_db),
+        skip: int = 0,
+        limit: int = 100,
+        current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
     """
     Retrieve users.
@@ -35,10 +35,10 @@ def read_users(
 
 @router.post("/", response_model=schemas.User)
 def create_user(
-    *,
-    db: Session = Depends(deps.get_db),
-    user_in: schemas.UserCreate,
-    # current_user: models.User = Depends(deps.get_current_active_superuser),
+        *,
+        db: Session = Depends(deps.get_db),
+        user_in: schemas.UserCreate,
+        # current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
     """
     Create new user.
@@ -59,41 +59,22 @@ def create_user(
 
 @router.put("/me", response_model=schemas.User)
 def update_user_me(
-    *,
-    db: Session = Depends(deps.get_db),
-    password: str = Body(None),
-    first_name: str = Body(None),
-    last_name: str = Body(None),
-    email: EmailStr = Body(None),
-    date_of_birth: date = Body(None),
-    phone_number: str = Body(None, example="+380123456789"),
-    current_user: models.User = Depends(deps.get_current_active_user),
+        *,
+        db: Session = Depends(deps.get_db),
+        user_in: schemas.UserUpdate,
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
     Update own user.
     """
-    current_user_data = jsonable_encoder(current_user)
-    user_in = schemas.UserUpdate(**current_user_data)
-    if password is not None:
-        user_in.password = password
-    if first_name is not None:
-        user_in.first_name = first_name
-    if last_name is not None:
-        user_in.last_name = last_name
-    if email is not None:
-        user_in.email = email
-    if date_of_birth is not None:
-        user_in.date_of_birth = date_of_birth
-    if phone_number is not None:
-        user_in.phone_number = phone_number
     user = crud.user.update(db, db_obj=current_user, obj_in=user_in)
     return user
 
 
 @router.get("/me", response_model=schemas.User)
 def read_user_me(
-    db: Session = Depends(deps.get_db),
-    current_user: models.User = Depends(deps.get_current_active_user),
+        db: Session = Depends(deps.get_db),
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
     Get current user.
@@ -103,14 +84,14 @@ def read_user_me(
 
 @router.post("/open", response_model=schemas.User)
 def create_user_open(
-    *,
-    db: Session = Depends(deps.get_db),
-    password: str = Body(...),
-    email: EmailStr = Body(...),
-    first_name: str = Body(None),
-    last_name: str = Body(None),
-    date_of_birth: date = Body(None),
-    phone_number: str = Body(None),
+        *,
+        db: Session = Depends(deps.get_db),
+        password: str = Body(...),
+        email: EmailStr = Body(...),
+        first_name: str = Body(None),
+        last_name: str = Body(None),
+        date_of_birth: date = Body(None),
+        phone_number: str = Body(None),
 ) -> Any:
     """
     Create new user without the need to be logged in.
@@ -138,9 +119,9 @@ def create_user_open(
 
 @router.get("/{user_id}", response_model=schemas.User)
 def read_user_by_id(
-    user_id: int,
-    current_user: models.User = Depends(deps.get_current_active_user),
-    db: Session = Depends(deps.get_db),
+        user_id: int,
+        current_user: models.User = Depends(deps.get_current_active_user),
+        db: Session = Depends(deps.get_db),
 ) -> Any:
     """
     Get a specific user by id.
@@ -157,11 +138,11 @@ def read_user_by_id(
 
 @router.put("/{user_id}", response_model=schemas.User)
 def update_user(
-    *,
-    db: Session = Depends(deps.get_db),
-    user_id: int,
-    user_in: schemas.UserUpdate,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
+        *,
+        db: Session = Depends(deps.get_db),
+        user_id: int,
+        user_in: schemas.UserUpdate,
+        current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
     """
     Update a user.
