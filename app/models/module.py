831012bce29df96cdeb9abbf7ba67bfe18635ae4
danraponga <dimasamehada@gmail.com>
2023-10-26 23:06:01 +0200

    data type of image field replaced from LargeBinary to String type, becouse we will store images at db as path

--- app/models/module.py
+++ app/models/module.py
@@ -7,5 +7,5 @@ from app.db.base_class import Base
 class Module(Base, TimestampedModel):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    image = Column(LargeBinary, nullable=True)
+    image = Column(String, nullable=True)
     
