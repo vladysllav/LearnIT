10590109aa2a3ce1cc39fcf79ca3c7c07fc7f400
danraponga <dimasamehada@gmail.com>
2023-10-29 01:27:54 +0200

    a foreign key to Course table has been added in Module table

--- app/models/module.py
+++ app/models/module.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, LargeBinary
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from app.models.base import TimestampedModel
 from app.db.base_class import Base
 
@@ -8,4 +9,6 @@ class Module(Base, TimestampedModel):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     image = Column(String, nullable=True)
+    course_id = Column(Integer, ForeignKey('course.id'))
+    course = relationship("Course", back_populates="modules")
     
