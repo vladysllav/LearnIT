16719a756d9413cdf85034a47c7676f1be1026c4
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-12-04 17:27:40 +0200

    fix router

--- app/api/courses/routes.py
+++ app/api/courses/routes.py
@@ -1,6 +1,10 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, Depends
 from sqlalchemy.orm import Session
 
+from app.dependencies.lessons import get_lessons
+from app.models import Lessons
 from app.models.user import User
 from app.models.course import Course
 from app.models.module import Module
@@ -8,13 +12,14 @@ from app.models.module import Module
 from app.crud.crud_course import course as crud_course
 from app.crud.crud_module import module as crud_module
 from app.schemas.course import CourseUpdate, CourseCreate
+from app.schemas.lessons import LessonsCreate, LessonsUpdate
 from app.schemas.module import ModuleCreate, ModuleUpdate
 
 from app.dependencies.base import get_db
 from app.dependencies.users import get_current_user
 from app.dependencies.course import get_course, check_course_access, get_module
 
-
+from app.crud.crud_lessons import lessons as crud_lessons
 
 router = APIRouter()
 
@@ -34,7 +39,7 @@ def read_course_by_id(course: Course = Depends(get_course)):
 def create_course(*, db: Session = Depends(get_db), course_in: CourseCreate,
                   current_user: User = Depends(get_current_user)):
     course = crud_course.create(db, obj_in=course_in, current_user=current_user)
-    
+
     return course
 
 
@@ -42,33 +47,30 @@ def create_course(*, db: Session = Depends(get_db), course_in: CourseCreate,
 def update_course(*, db: Session = Depends(get_db),
                   course: Course = Depends(get_course),
                   course_in: CourseUpdate):
-   
     course = crud_course.update(db, db_obj=course, obj_in=course_in)
     return course
+
 
 @router.get('/{course_id}/modules/{module_id}')
 def read_module_by_id(course: Course = Depends(get_course),
                       module: Module = Depends(get_module)):
-    
     return module
-    
-    
+
+
 @router.get('/{course_id}/modules/')
 def read_all(db: Session = Depends(get_db),
              course: Course = Depends(get_course),
              skip: int = 0, limit: int = 100):
-    
     modules = crud_module.get_list(db, skip=skip, limit=limit)
     return modules
-    
-    
+
+
 @router.post('/{course_id}/modules/')
 def create_module(*, db: Session = Depends(get_db),
                   course_if_access: None = Depends(check_course_access),
                   module_in: ModuleCreate,
                   current_user: User = Depends(get_current_user),
                   course_id: int):
-
     module = crud_module.create(db, obj_in=module_in, course_id=course_id,
                                 current_user=current_user)
     return module
@@ -78,7 +80,6 @@ def create_module(*, db: Session = Depends(get_db),
 def update_module(*, db: Session = Depends(get_db), module_in: ModuleUpdate,
                   course_access: None = Depends(check_course_access),
                   module: Module = Depends(get_module)):
-    
     module = crud_module.update(db, db_obj=module, obj_in=module_in)
     return module
 
@@ -87,6 +88,54 @@ def update_module(*, db: Session = Depends(get_db), module_in: ModuleUpdate,
 def remove_module(*, db: Session = Depends(get_db),
                   course_access: None = Depends(check_course_access),
                   module_id: int):
-    
     module = crud_module.remove(db, id=module_id)
     return module
+
+
+@router.post("/{module_id}/lessons/{lessons_id}")
+def create_lessons(*, db: Session = Depends(get_db), lesson_in: LessonsCreate,
+                   current_user: User = Depends(get_current_user),
+                   module: Module = Depends(get_module),
+                   module_id: int):
+    lesson = crud_lessons.create(db, obj_in=lesson_in, current_user=current_user, module_id=module_id)
+    return lesson
+
+
+@router.get('/{module_id}/lessons/{lessons_id}')
+def get_lessons(lessons: Lessons = Depends(get_lessons),
+                module: Module = Depends(get_module),):
+    return lessons
+
+
+@router.get('/modules/{module_id}/lessons/')
+def get_lessons_all(db: Session = Depends(get_db),
+             module: Module = Depends(get_module),
+             skip: int = 0, limit: int = 100):
+    lessons = crud_lessons.get_list(db, skip=skip, limit=limit)
+    return lessons
+
+
+@router.put('/{module_id}/lessons/{lesson_id}')
+def update_lesson(*, db: Session = Depends(get_db),
+                  module: Module = Depends(get_module),
+                  lesson: Lessons = Depends(get_lessons),
+                  lesson_in: LessonsUpdate):
+    lesson = crud_lessons.update(db, db_obj=lesson, obj_in=lesson_in)
+    return lesson
+
+
+@router.patch('/{module_id}/lessons/{lesson_id}')
+def update_lesson(*, db: Session = Depends(get_db),
+                  module: Module = Depends(get_module),
+                  lesson: Lessons = Depends(get_lessons),
+                  lesson_in: LessonsUpdate):
+    lesson = crud_lessons.update(db, db_obj=lesson, obj_in=lesson_in)
+    return lesson
+
+
+@router.delete('/{module_id}/lessons/{lesson_id}')
+def delete_lesson(*, db: Session = Depends(get_db),
+                  module: Module = Depends(get_module),
+                  lesson_id: int):
+    lessons = crud_lessons.remove(db, id=lesson_id)
+    return lessons
