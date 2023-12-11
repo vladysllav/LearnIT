0b4b95a3be418701449c8e344fd24593aa59adf9
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-12-11 12:02:54 +0200

    fix after review 11.12.2023

--- app/api/courses/routes.py
+++ app/api/courses/routes.py
@@ -22,6 +22,7 @@ from app.dependencies.course import get_course, check_course_access, get_module
 from app.crud.crud_lessons import lessons as crud_lessons
 
 router = APIRouter()
+allow_create_resource = PermissionChecker([UserType.admin, UserType.superadmin])
 
 
 @router.get('/')
@@ -35,23 +36,23 @@ def read_course_by_id(course: Course = Depends(get_course)):
     return course
 
 
-@router.post('/')
+@router.post('/',dependencies=[Depends(allow_create_resource)])
 def create_course(*, db: Session = Depends(get_db), course_in: CourseCreate,
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  current_user: User = Depends(get_current_user)):
     course = crud_course.create(db, obj_in=course_in, current_user=current_user)
+
     return course
 
 
-@router.put('/{course_id}')
+@router.put('/{course_id}', dependencies=[Depends(allow_create_resource)])
 def update_course(*, db: Session = Depends(get_db),
                   course: Course = Depends(get_course),
-                  course_in: CourseUpdate,
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  course_in: CourseUpdate):
     course = crud_course.update(db, db_obj=course, obj_in=course_in)
     return course
 
 
-@router.get('/{course_id}/modules/{module_id}')
+@router.get('/{course_id}/modules/{module_id}' , )
 def read_module_by_id(course: Course = Depends(get_course),
                       module: Module = Depends(get_module)):
     return module
@@ -65,41 +66,39 @@ def read_all(db: Session = Depends(get_db),
     return modules
 
 
-@router.post('/{course_id}/modules/')
+@router.post('/{course_id}/modules/', dependencies=[Depends(allow_create_resource)])
 def create_module(*, db: Session = Depends(get_db),
                   course_if_access: None = Depends(check_course_access),
                   module_in: ModuleCreate,
-                  course_id: int,
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  current_user: User = Depends(get_current_user),
+                  course_id: int):
     module = crud_module.create(db, obj_in=module_in, course_id=course_id,
                                 current_user=current_user)
     return module
 
 
-@router.patch('/{course_id}/modules/{module_id}')
+@router.patch('/{course_id}/modules/{module_id}',dependencies=[Depends(allow_create_resource)])
 def update_module(*, db: Session = Depends(get_db), module_in: ModuleUpdate,
                   course_access: None = Depends(check_course_access),
-                  module: Module = Depends(get_module),
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  module: Module = Depends(get_module)):
     module = crud_module.update(db, db_obj=module, obj_in=module_in)
     return module
 
 
 
-@router.delete('/{course_id}/modules/{module_id}')
+@router.delete('/{course_id}/modules/{module_id}',dependencies=[Depends(allow_create_resource)])
 def remove_module(*, db: Session = Depends(get_db),
                   course_access: None = Depends(check_course_access),
-                  module_id: int,
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  module_id: int):
     module = crud_module.remove(db, id=module_id)
     return module
 
 
-@router.post("/{course_id}/modules/{module_id}/lessons/{lessons_id}")
+@router.post("/{course_id}/modules/{module_id}/lessons/{lessons_id}",dependencies=[Depends(allow_create_resource)])
 def create_lessons(*, db: Session = Depends(get_db), lesson_in: LessonsCreate,
+                   current_user: User = Depends(get_current_user),
                    module: Module = Depends(get_module),
-                   course_access: None = Depends(check_course_access),
-                   current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                   course_access: None = Depends(check_course_access)):
     lesson = crud_lessons.create(db, obj_in=lesson_in, current_user=current_user, module_id=module.id)
     return lesson
 
@@ -119,33 +118,30 @@ def get_lessons_all(db: Session = Depends(get_db),
     return lessons
 
 
-@router.put('/{course_id}/modules/{module_id}/lessons/{lesson_id}')
+@router.put('/{course_id}/modules/{module_id}/lessons/{lesson_id}',dependencies=[Depends(allow_create_resource)])
 def update_lesson(*, db: Session = Depends(get_db),
                   module: Module = Depends(get_module),
                   lesson: Lessons = Depends(get_lessons),
                   lesson_in: LessonsUpdate,
-                  course_access: None = Depends(check_course_access),
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  course_access: None = Depends(check_course_access)):
     lesson = crud_lessons.update(db, db_obj=lesson, obj_in=lesson_in)
     return lesson
 
 
-@router.patch('/{course_id}/modules/{module_id}/lessons/{lesson_id}')
+@router.patch('/{course_id}/modules/{module_id}/lessons/{lesson_id}',dependencies=[Depends(allow_create_resource)])
 def update_lesson(*, db: Session = Depends(get_db),
                   module: Module = Depends(get_module),
                   lesson: Lessons = Depends(get_lessons),
                   lesson_in: LessonsUpdate
-                  ,course_access: None = Depends(check_course_access),
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  ,course_access: None = Depends(check_course_access)):
     lesson = crud_lessons.update(db, db_obj=lesson, obj_in=lesson_in)
     return lesson
 
 
-@router.delete('/{course_id}/modules/{module_id}/lessons/{lesson_id}')
+@router.delete('/{course_id}/modules/{module_id}/lessons/{lesson_id}',dependencies=[Depends(allow_create_resource)])
 def delete_lesson(*, db: Session = Depends(get_db),
                   module: Module = Depends(get_module),
                   lesson_id: int,
-                  course_access: None = Depends(check_course_access),
-                  current_user: User = Depends(PermissionChecker([UserType.admin, UserType.superadmin]))):
+                  course_access: None = Depends(check_course_access)):
     lessons = crud_lessons.remove(db, id=lesson_id)
     return lessons
